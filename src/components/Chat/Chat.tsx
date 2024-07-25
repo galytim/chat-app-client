d7ff0b47a0cf74f9289212e254051cff411b7357
Богдана Бадьянова <badyanova.bogdana@gmail.com>
2024-07-25 23:16:16 +0300

    Add prettier, husky, pre-commit lint-staged

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,18 +1,17 @@
-import './Chat.scss';
-import ChatService from '../../services/Chat.service';
-import { useEffect, useState } from 'react';
-import React from 'react';
+import "./Chat.scss";
+import ChatService from "../../services/Chat.service";
+import { useEffect, useState } from "react";
+import React from "react";
 
-const Chat = (props :any) => {
-    const [chats, setChats] = useState<any[]>([]);
+const Chat = (props: any) => {
+  const [chats, setChats] = useState<any[]>([]);
 
-    return (
-        <div className="Chat">
-            <div>{props.chat.email}</div>
-            <div>{props.chat.name}</div>
-
-        </div>
-    );
-}
+  return (
+    <div className="Chat">
+      <div>{props.chat.email}</div>
+      <div>{props.chat.name}</div>
+    </div>
+  );
+};
 
 export default Chat;
--- src/services/Chat.service.ts
+++ src/services/Chat.service.ts
@@ -1,11 +1,10 @@
 import axios from "axios";
 
-class ChatService{
-    static async getComments() {
-        const apiUrl: string = 'https://jsonplaceholder.typicode.com/';
-        return await axios.get(`${apiUrl}/comments?_limit=10`);
-    }
-
+class ChatService {
+  static async getComments() {
+    const apiUrl: string = "https://jsonplaceholder.typicode.com/";
+    return await axios.get(`${apiUrl}/comments?_limit=10`);
+  }
 }
 
 export default ChatService;
